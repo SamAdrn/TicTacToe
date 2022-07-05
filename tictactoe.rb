8a460cbf86e47e6a3fe8cc9c853b3565424896fd
Samuel Kosasih <75837551+SamAdrn@users.noreply.github.com>
2022-07-05 03:26:41 -0400

    Update tictactoe.rb
    - Changed the p parameter in most functions to pers, for more clarity.
    
    - Added the EASY mode for vsCPU games.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,10 @@
 module TicTacToe
   SYMS = { 0 => " ", 1 => "X", 2 => "O" }
 
+  EASY = 1
+  MED = 2
+  HARD = 3
+
   def print_board(board)
     ["+-----------+\n",
      "| #{SYMS[board[0]]} | #{SYMS[board[1]]} | #{SYMS[board[2]]} |\n",
@@ -11,22 +15,24 @@ module TicTacToe
      "+-----------+\n"]
   end
 
-  def get_move(board, player, p, mode)
-    best = p == player ? -100 : 100
+  def get_move(board, player, pers, mode)
+    return 0.upto(8).select { |i| board[i] == 0 }.sample if (mode == EASY)
+
+    best = pers == player ? -100 : 100
     move = -1
     0.upto(8) do |i|
       if (board[i] == 0)
         # simulate board where p makes a move in m
-        board[i] = p
+        board[i] = pers
 
         # store the score based on a sequence of moves starting with m.
         # The recursive call is made on the alternate perspective.
-        score = minimax(board, 0, player, p == 1 ? 2 : 1, mode)
+        score = minimax(board, 0, player, pers == 1 ? 2 : 1, mode)
 
         # revert back to original board
         board[i] = 0
 
-        if (p == player)
+        if (pers == player)
           if (score >= best)
             move = i
             best = score
@@ -46,7 +52,7 @@ module TicTacToe
   # X wins: 10,   O wins: -10,    tie: 0
   # player = human, opp = AI
   # p: perspective
-  def minimax(board, depth, player, p, mode)
+  def minimax(board, depth, player, pers, mode)
 
     # check for available spots on the board
     spots = 0.upto(8).select { |i| board[i] == 0 }
@@ -65,14 +71,14 @@ module TicTacToe
     # initialize array of scores
     scores = []
 
-    if (mode != 2 || depth <= 1)
+    if (mode != MED || depth <= 1)
       spots.each do |m|
         # simulate board where p makes a move in m
-        board[m] = p
+        board[m] = pers
 
         # Store the score based on a sequence of moves starting with m.
         # The recursive call is made on the alternate perspective.
-        scores << minimax(board, depth + 1, player, p == 1 ? 2 : 1, mode)
+        scores << minimax(board, depth + 1, player, pers == 1 ? 2 : 1, mode)
 
         # revert back to original board
         board[m] = 0
@@ -81,17 +87,17 @@ module TicTacToe
 
     # find optimal move for player based on perspective
     return 0 if scores.empty?
-    return p == player ? scores.max : scores.min
+    return pers == player ? scores.max : scores.min
   end
 
-  def win?(board, p)
-    ((board[0] == p && board[1] == p && board[2] == p) ||
-     (board[3] == p && board[4] == p && board[5] == p) ||
-     (board[6] == p && board[7] == p && board[8] == p) ||
-     (board[0] == p && board[3] == p && board[6] == p) ||
-     (board[1] == p && board[4] == p && board[7] == p) ||
-     (board[2] == p && board[5] == p && board[8] == p) ||
-     (board[0] == p && board[4] == p && board[8] == p) ||
-     (board[2] == p && board[4] == p && board[6] == p)) ? true : false
+  def win?(board, pers)
+    ((board[0] == pers && board[1] == pers && board[2] == pers) ||
+     (board[3] == pers && board[4] == pers && board[5] == pers) ||
+     (board[6] == pers && board[7] == pers && board[8] == pers) ||
+     (board[0] == pers && board[3] == pers && board[6] == pers) ||
+     (board[1] == pers && board[4] == pers && board[7] == pers) ||
+     (board[2] == pers && board[5] == pers && board[8] == pers) ||
+     (board[0] == pers && board[4] == pers && board[8] == pers) ||
+     (board[2] == pers && board[4] == pers && board[6] == pers)) ? true : false
   end
 end
