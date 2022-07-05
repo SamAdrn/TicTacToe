4c437283c0272f0ffeec8b0fe2448d94937a7c21
Samuel Kosasih <75837551+SamAdrn@users.noreply.github.com>
2022-07-05 03:25:55 -0400

    Update runner.rb
    - Added PVP mode. Now two players can play against each other.
    
    - Added an introduction
    
    - Changed the wording for the play instructions so it can also be used in PVP mode
    
    - Added different mode functionalities when playing against the CPU
    
    - Allow players to try again immediately after every game.

--- runner.rb
+++ runner.rb
@@ -2,19 +2,31 @@ require_relative "tictactoe.rb"
 
 include TicTacToe
 
-EASY = 1
-MEDIUM = 2
-HARD = 3
+QUIT = /q(uit)?/
+
+INTRO =
+  ["=================================================================\n",
+   "|          OOXX      Welcome to TIC TAC TOE      OXOX           |\n",
+   "=================================================================\n",
+   "|                    made by: Samuel Kosasih                    |\n",
+   "=================================================================\n",
+   "| Choose a mode:                                                |\n",
+   "| <cas>  : play a casual game with the CPU                      |\n",
+   "| <pro>  : battle an expert CPU                                 |\n",
+   "| <GOD>  : CPU IS A GOD                                         |\n",
+   "| <pvp>  : beat your friends (and vice versa)                   |\n",
+   "| <quit> : quit the game (works anywhere in the game)           |\n",
+   "=================================================================\n"]
 
 PLAY_INS =
   ["=================================================================\n",
    "|  +-----------+  | An example board is shown on the left.      |\n",
    "|  | 1 | 2 | 3 |  |                                             |\n",
    "|  +-----------+  | To play, simply enter the desired grid when |\n",
-   "|  | 4 | 5 | 6 |  | it is your turn.  Enter <q> to quit.        |\n",
+   "|  | 4 | 5 | 6 |  | it is your turn. Choices are grids 1 - 9.   |\n",
    "|  +-----------+  |                                             |\n",
-   "|  | 7 | 8 | 9 |  |                                             |\n",
-   "|  +-----------+  | Now, enter <1> to be X, or <2> to be O      |\n",
+   "|  | 7 | 8 | 9 |  | Enter <q> to quit anytime.                  |\n",
+   "|  +-----------+  | GOOD LUCK!                                  |\n",
    "=================================================================\n"]
 
 # ============================================================================
@@ -87,14 +99,16 @@ end
 
 # ============================================================================
 
-def play
+def playCPU(mode)
   board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
   puts
+  f_print(["VS CPU (#{mode == EASY ? "CASUAL" :
+    mode == MED ? "PROFESSIONAL" : "GOD"})\n"], 0, false)
   f_print(PLAY_INS, 0, true)
 
   # Choose Symbols
   while (true)
-    puts
+    f_print(["Now, enter <1> to be X, or <2> to be O\n"], 0, false)
     print "Choose Symbol => "
     inp = STDIN.gets.chomp.downcase
 
@@ -102,6 +116,8 @@ def play
       player = inp.to_i
       cpu = player == 1 ? 2 : 1
       break
+    elsif (inp =~ QUIT)
+      return -1
     else
       puts "Invalid Input. Please enter <1> to be X or <2> to be O ..."
       next
@@ -137,8 +153,8 @@ def play
         else
           board[inp.to_i - 1] = player
         end
-      elsif (inp =~ /q(uit)?/)
-        return
+      elsif (inp =~ QUIT)
+        return -1
       else
         puts "Invalid Input. Enter a number between 1 - 9 ...\n\n"
         f_print(print_board(board), 0, false)
@@ -147,7 +163,7 @@ def play
     else # cpu turn
       f_print(["\n", "--- COMPUTER's TURN ---\n"], 0, false)
       delay(1)
-      board[get_move(board, player, cpu, 3)] = cpu
+      board[get_move(board, player, cpu, mode)] = cpu
     end
 
     turn = turn == 1 ? 2 : 1
@@ -156,17 +172,92 @@ def play
   end
 end
 
-play
+def playPVP
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  puts
+  f_print(["PVP MODE\n"], 0, false)
+  f_print(PLAY_INS, 0, true)
+  delay(1)
 
-# board = [0, 1, 0,
-#          0, 0, 1,
-#          2, 2, 1]
-# move = get_move(board, 1, 2)
+  turn = 1
+  f_print(print_board(board), 0, false)
 
-# board = [0, 2, 0,
-#          0, 0, 2,
-#          1, 1, 2]
-# move = get_move(board, 2, 1)
-# puts
-# puts "move: #{move}"
-# f_print(print_board(board), 0)
+  while (true)
+    # check for winning or tie combinations
+    flag = win?(board, 1) ? 1 :
+      win?(board, 2) ? 2 :
+      !board.include?(0) ? 3 : nil
+
+    if flag # end game
+      f_print(["\n", "--- PLAYER 1 (X) WINS ---\n"], 0, false) if flag == 1
+      f_print(["\n", "--- PLAYER 2 (O) WINS ---\n"], 0, false) if flag == 2
+      f_print(["\n", "--- TIE ---\n"], 0, false) if flag == 3
+      break
+    end
+
+    f_print(["\n", "--- PLAYER #{turn}'s (#{SYMS[turn]}) TURN ---\n"], 0, false)
+    print "Choose a spot => "
+    inp = STDIN.gets.chomp.downcase
+
+    if (inp =~ /^[1-9]$/)
+      if (board[inp.to_i - 1] != 0)
+        puts "This grid is taken. Choose an empty one.\n\n"
+        f_print(print_board(board), 0, false)
+        next
+      else
+        board[inp.to_i - 1] = turn
+      end
+    elsif (inp =~ QUIT)
+      return -1
+    else
+      puts "Invalid Input. Enter a number between 1 - 9 ...\n\n"
+      f_print(print_board(board), 0, false)
+      next
+    end
+
+    turn = turn == 1 ? 2 : 1
+    puts "\n\n"
+    f_print(print_board(board), 0, false)
+  end
+end
+
+def main
+  while (true)
+    puts
+    f_print(INTRO, 0, false)
+    puts
+    print "Enter your command => "
+    inp = STDIN.gets.chomp.downcase
+
+    if inp =~ /c(as)?|p(ro)?|g(od)?|pvp|/
+      if (inp =~ /pvp/)
+        while (true)
+          if (playPVP() != -1)
+            delay(1.5)
+            f_print(["Try Again?  Y/N => "], 0, false)
+            inp = STDIN.gets.chomp.downcase
+            if inp =~ /y(es)?/ then next else break end
+          end
+          break
+        end
+      else
+        mode = inp =~ /c(as)?/ ? EASY : inp =~ /p(ro)?/ ? MED : HARD
+        while (true)
+          if (playCPU(mode) != -1)
+            delay(1.5)
+            f_print(["Try Again?  Y/N => "], 0, false)
+            inp = STDIN.gets.chomp.downcase
+            if inp =~ /y(es)?/ then next else break end
+          end
+          break
+        end
+      end
+    elsif inp =~ QUIT
+      break
+    else
+      puts "Invalid Input. Please try again ..."
+    end
+  end
+end
+
+main()
