d001371304d2038ce71e0481d906fbf2f811b1d3
Samuel Kosasih <75837551+SamAdrn@users.noreply.github.com>
2022-07-16 23:29:46 +0700

    Update runner.rb
    Completed Documentation

--- runner.rb
+++ runner.rb
@@ -3,8 +3,22 @@ require "colorize"
 
 include TicTacToe
 
+# A regex to match the strings q or quit.
+# Used to indicate when the user decides to quit the program. It appears in
+# most areas of the source code, so a constant is made for it.
 QUIT = /q(uit)?/
 
+# Displays the possible modes for the user to choose from, mainly the different
+# difficulties for playing against the CPU and a PVP mode. Choices include:
+# - +<cas>+  : Easy. CPU picks moves randomly based on available spots
+# - +<pro>+  : Medium. CPU uses a modified minimax algorithm to look two steps
+#   ahead.
+# - +<god>+  : Impossible. CPU uses the minimax algorithm to simulate a perfect
+#   player.
+# - +<pvp>+  : Allows users to play against a friend locally.
+# - +<quit>+ : Quits the program.
+#
+# @see #minimax
 INTRO =
   ["=================================================================\n",
    "|          " + "OOXX".bold.blink +
@@ -32,6 +46,8 @@ INTRO =
    " the game (works anywhere in the game)           |\n",
    "=================================================================\n"]
 
+# Displays instructions on how to play the game, while also providing an
+# example of a board on the left.
 PLAY_INS =
   ["=================================================================\n",
    "|  +-----------+  | An example board is shown on the left.      |\n",
@@ -135,8 +151,25 @@ end
 
 # ----------------------------------------------------------------------------
 
+# Simulates a game of Tic Tac Toe against the CPU. The algorithm alternates
+# turns between the user and the CPU until a winning combination is found or
+# all spots on the board has been filled (indicating a tie).
+#
+# The parameter +mode+ allows different levels of the CPU. Possible values are
+# described below:
+# - +1+ : Easy mode.
+# - +2+ : Medium mode.
+# - +3+ : Impossible mode.
+#
+# @param mode [Integer] indicates the level of the CPU
+#
+# @return [void]
+#
+# @see #minimax
 def playCPU(mode)
-  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]    # creates an empty board for a game
+
+  # displays level of CPU chosen followed by game instructions
   f_print(
     ["                                                                 \n",
      "#{mode == EASY ?
@@ -147,7 +180,7 @@ def playCPU(mode)
       PLAY_INS, 0, true
   )
 
-  # Choose Symbols
+  # lets the player choose which symbol they want to be
   while (true)
     f_print(
       ["                                                                 \n",
@@ -158,6 +191,7 @@ def playCPU(mode)
     print "Choose Symbol => "
     inp = STDIN.gets.chomp.downcase
 
+    # retrieves user's choice and binds the chosen symbol
     if (inp =~ /^[1-2]$/)
       player = inp.to_i
       cpu = player == 1 ? 2 : 1
@@ -172,12 +206,13 @@ def playCPU(mode)
     end
   end
 
+  # randomly selects between the user and the CPU to start the game
   turn = rand(1..2)
   f_print(print_board(board), 0, false)
 
+  # alternate between the player and CPU until the game ends
   while (true)
-    # check for winning or tie combinations
-    flag = win?(board, player) ? 1 :
+    flag = win?(board, player) ? 1 : # check for winning or tie combinations
       win?(board, cpu) ? 2 :
       !board.include?(0) ? 3 : nil
 
@@ -200,39 +235,40 @@ def playCPU(mode)
       break
     end
 
-    if (turn == player) # player turn
+    if (turn == player) # player's turn
       f_print(
         ["                                                                 \n",
          "                        --- " + "YOUR TURN".light_magenta +
          " ---\n"], 0, true
       )
+
       print "Choose a spot => "
       inp = STDIN.gets.chomp.downcase
 
-      if (inp =~ /^[1-9]$/)
-        if (board[inp.to_i - 1] != 0)
+      if (inp =~ /^[1-9]$/) # valid input
+        if (board[inp.to_i - 1] != 0) # invalid spot
           puts "This spot is " + "taken".light_red +
                  ". Choose an empty one.\n\n"
           f_print(print_board(board), 0, false)
           next
-        else
+        else # valid spot
           board[inp.to_i - 1] = player
         end
-      elsif (inp =~ QUIT)
+      elsif (inp =~ QUIT) # quits game
         return -1
-      else
+      else # invalid input
         puts "Invalid Input".light_red +
                ". Enter a number between 1 - 9 ...\n\n"
         f_print(print_board(board), 0, false)
         next
       end
-    else # cpu turn
+    else # cpu's turn
       f_print(["\n", "--- COMPUTER's TURN ---\n"], 0, false)
       delay(1)
       board[get_move(board, player, cpu, mode)] = cpu
     end
 
-    turn = turn == 1 ? 2 : 1
+    turn = turn == 1 ? 2 : 1 # switch turns
     puts "\n\n"
     f_print(print_board(board), 0, false)
   end
@@ -240,8 +276,17 @@ end
 
 # ----------------------------------------------------------------------------
 
+# Simulates a game of Tic Tac Toe for the player to play against another player
+# locally. Alternates between Player 1 (X) and Player 2 (O), with both players
+# always bounded by their symbols and Player 1 (X) starting first.
+#
+# @return [void]
+#
+# @see #minimax
 def playPVP
-  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0] # creates an empty board for a game
+
+  # displays game mode chosen followed by game instructions
   f_print(
     ["                                                                 \n",
      "                            " + "PVP MODE".light_cyan + "\n"] +
@@ -249,12 +294,12 @@ def playPVP
   )
   delay(1)
 
+  # denote player 1 to be the player who starts the game
   turn = 1
   f_print(print_board(board), 0, false)
 
   while (true)
-    # check for winning or tie combinations
-    flag = win?(board, 1) ? 1 :
+    flag = win?(board, 1) ? 1 : # check for winning or tie combinations
       win?(board, 2) ? 2 :
       !board.include?(0) ? 3 : nil
 
@@ -282,31 +327,33 @@ def playPVP
        "                   --- " + "PLAYER 1's (X) TURN".light_magentan +
        " ---\n"], 0, true
     ) if turn == 1
+
     f_print( # Prompt Player 2
       ["                                                                 \n",
        "                   --- " + "PLAYER 2's (O) TURN".light_cyan +
        " ---\n"], 0, true
     ) if turn == 2
+
     print "Choose a spot => "
     inp = STDIN.gets.chomp.downcase
 
-    if (inp =~ /^[1-9]$/)
-      if (board[inp.to_i - 1] != 0)
+    if (inp =~ /^[1-9]$/) # valid input
+      if (board[inp.to_i - 1] != 0) # invalid spot
         puts "This spot is " + "taken".light_red + ". Choose an empty one.\n\n"
         f_print(print_board(board), 0, false)
         next
-      else
+      else # valid spot
         board[inp.to_i - 1] = turn
       end
-    elsif (inp =~ QUIT)
+    elsif (inp =~ QUIT) # quits game
       return -1
-    else
+    else # invalid input
       puts "Invalid Input".light_red + ". Enter a number between 1 - 9 ...\n\n"
       f_print(print_board(board), 0, false)
       next
     end
 
-    turn = turn == 1 ? 2 : 1
+    turn = turn == 1 ? 2 : 1 # switch turn
     puts "\n\n"
     f_print(print_board(board), 0, false)
   end
@@ -320,16 +367,25 @@ end
 
 # ----------------------------------------------------------------------------
 
+# Main entry point of the game.
+#
+# As the game starts, the player is able to choose between game modes. Playing
+# against the CPU will call {#playCPU} with the appropriate mode. Alternatively,
+# player vs. player mode will call {#playPVP}.
+#
+# @return [void]
 def main
   while (true)
+    # displays instructions for the player to choose between game modes
     puts
     f_print(INTRO, 0, true)
     puts
     print "Enter your command => "
     inp = STDIN.gets.chomp.downcase
 
-    if inp =~ /c(as)?|p(ro)?|g(od)?|pvp/
-      if (inp =~ /pvp/)
+    if inp =~ /c(as)?|p(ro)?|g(od)?|pvp/ # valid game modes
+      if (inp =~ /pvp/) # PVP mode
+        # used a loop here to allow a new game to be started immediately
         while (true)
           if (playPVP() != -1)
             puts
@@ -339,9 +395,12 @@ def main
             if inp =~ /y(es)?/ then next else break end
           end
           break
-        end
+        end # vs CPU mode
       else
+        # selects the appropriate mode
         mode = inp =~ /c(as)?/ ? EASY : inp =~ /p(ro)?/ ? MED : HARD
+
+        # used a loop here to allow a new game to be started immediately
         while (true)
           if (playCPU(mode) != -1)
             puts
@@ -353,10 +412,10 @@ def main
           break
         end
       end
-    elsif inp =~ QUIT
-        puts "see you soon!\n".light_green
+    elsif inp =~ QUIT # quits game
+      puts "see you soon!\n".light_green
       break
-    else
+    else # invalid input
       puts "Invalid Input".light_red + ". Please try again ..."
     end
   end
